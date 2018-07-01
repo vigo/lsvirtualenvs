c6d337dd8a32e7cf2e5be22631d08ffd29642667
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2018-07-01 21:56:29 +0300

    Code refactoring

--- lsvirtualenvs.go
+++ lsvirtualenvs.go
@@ -1,7 +1,14 @@
+/*
+
+	Build with: go version go1.10.3 darwin/amd64
+	Created by Uğur "vigo" Özyılmazel on 2018-07-01.
+
+*/
 package main
 
 import (
 	"bytes"
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -11,16 +18,6 @@ import (
 	"strconv"
 	"strings"
 	"sync"
-)
-
-const (
-	VERSION      = "0.1.0"
-	COLOR_GREEN  = "\x1b[32m"
-	COLOR_YELLOW = "\x1b[33m"
-	COLOR_WHITE  = "\x1b[37m"
-	COLOR_GRAY   = "\x1b[38m"
-
-	COLOR_RESET = "\x1b[0m"
 )
 
 var (
@@ -33,7 +30,7 @@ Options:
 
   -h, --help      Display help! :)
   -c, --color     Enable color output
-  -s, --simple    Just list environment names
+  -s, --simple    Just list environment names, overrides -c, -i
   -i, --index     Add index number to output
       --version   Version information
 
@@ -45,65 +42,124 @@ Examples:
   lsvirtualenvs --color
   lsvirtualenvs -c -i
   lsvirtualenvs --color --index
+
   lsvirtualenvs -s
   lsvirtualenvs -simple
 
 `
-	optionsColorOutput  *bool
-	optionsSimpleOutput *bool
-	optionsIndexEnabled *bool
-	optionsVersion      *bool
-	indexDigits         int
+
+	cmdOptionColorOutput  *bool
+	cmdOptionSimpleOutput *bool
+	cmdOptionIndexEnabled *bool
+	cmdOptionVersionInfo  *bool
+	maxIndexDigits        int
+
+	outputHeader = `You have %s %s available
+
+`
 )
 
-func colorPrint(color string, text string) string {
-	return fmt.Sprintf("%s%s%s", color, text, COLOR_RESET)
-}
+const (
+	VERSION = "0.1.0"
 
-func rightPadding(text string, length int) string {
-	strRepeat := strings.Repeat(".", length-len(text))
-	strOut := fmt.Sprintf("[%s] %s", text, strRepeat)
+	COLOR_RED     = "\x1b[31m"
+	COLOR_GREEN   = "\x1b[32m"
+	COLOR_YELLOW  = "\x1b[33m"
+	COLOR_BLUE    = "\x1b[34m"
+	COLOR_MAGENTA = "\x1b[35m"
+	COLOR_CYAN    = "\x1b[36m"
+	COLOR_WHITE   = "\x1b[37m"
+	COLOR_GRAY    = "\x1b[38m"
 
-	if *optionsColorOutput {
-		strOut = fmt.Sprintf("[%s] %s", colorPrint(COLOR_YELLOW, text), colorPrint(COLOR_GRAY, strRepeat))
-	}
-	return strOut
-}
+	COLOR_RESET = "\x1b[0m"
+)
 
-func calculateIndex(number int) string {
-	return fmt.Sprintf("%0*d", indexDigits, number+1)
-}
-
-func main() {
+// init implements commandline flag parsing
+func init() {
 	flag.Usage = func() {
 		fmt.Fprint(os.Stderr, usage)
 	}
 
-	optionsColorOutput = flag.Bool("color", false, "")
-	flag.BoolVar(optionsColorOutput, "c", false, "")
+	cmdOptionColorOutput = flag.Bool("color", false, "")
+	flag.BoolVar(cmdOptionColorOutput, "c", false, "")
 
-	optionsSimpleOutput = flag.Bool("simple", false, "")
-	flag.BoolVar(optionsSimpleOutput, "s", false, "")
+	cmdOptionSimpleOutput = flag.Bool("simple", false, "")
+	flag.BoolVar(cmdOptionSimpleOutput, "s", false, "")
 
-	optionsIndexEnabled = flag.Bool("index", false, "")
-	flag.BoolVar(optionsIndexEnabled, "i", false, "")
+	cmdOptionIndexEnabled = flag.Bool("index", false, "")
+	flag.BoolVar(cmdOptionIndexEnabled, "i", false, "")
 
-	optionsVersion = flag.Bool("version", false, "")
-
+	cmdOptionVersionInfo = flag.Bool("version", false, "")
 	flag.Parse()
+}
 
-	if *optionsVersion {
+// printColorf implements terminal friendly color output if
+// color enable flag is set!
+func printColorf(text string, color string) string {
+	if *cmdOptionColorOutput {
+		return fmt.Sprintf("%s%s%s", color, text, COLOR_RESET)
+	} else {
+		return text
+	}
+
+}
+
+// checkEnvVar checks given environment variable if exists
+// returns error unless exists
+func checkEnvVar(envVar string) (string, error) {
+	v := os.Getenv(envVar)
+	if v == "" {
+		return "", errors.New("$WORKON_HOME does't exists in your environment!")
+	}
+	return v, nil
+}
+
+// pluralize implements a quick and dirty pluralize process
+// if you pass plural version as "s" only, output will be
+// suffixed with "s" infront of singular version of text.
+func pluralize(singular string, plural string, amount int) string {
+	out := singular
+	if amount > 1 {
+		if plural == "s" {
+			out = fmt.Sprintf("%ss", singular)
+		} else {
+			out = plural
+		}
+	}
+	return out
+}
+
+// dynamicDigitPadding implements dynamic string format with
+// digit padding
+func dynamicDigitPadding(number int) string {
+	return fmt.Sprintf("%0*d", maxIndexDigits, number+1)
+}
+
+// rightPaddingWithChar implements padding for given length with given padding
+// character.
+func rightPaddingWithChar(text string, length int, padChar string, format string) string {
+	if padChar == "" {
+		padChar = "."
+	}
+
+	repeatingChars := strings.Repeat(padChar, length-len(text))
+	return fmt.Sprintf(format, text, repeatingChars)
+}
+
+func main() {
+	if *cmdOptionVersionInfo {
 		fmt.Fprint(os.Stdout, fmt.Sprintf("%s\n", VERSION))
 		os.Exit(0)
 	}
 
-	workonHome := os.Getenv("WORKON_HOME")
-	if workonHome == "" {
-		fmt.Fprint(os.Stderr, "$WORKON_HOME does't exists in your environment!")
+	envWorkonHome, err := checkEnvVar("WORKON_HOME")
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	files, err := ioutil.ReadDir(workonHome)
+	// read all files under envWorkonHome
+	filesList, err := ioutil.ReadDir(envWorkonHome)
 	if err != nil {
 		fmt.Fprint(os.Stderr, err)
 		os.Exit(1)
@@ -112,9 +168,9 @@ func main() {
 	var wg sync.WaitGroup
 	var lock = sync.RWMutex{}
 
-	result := make(map[string]string)
+	virtualEnvsList := make(map[string]string)
 
-	for _, file := range files {
+	for _, file := range filesList {
 		if file.IsDir() == true {
 			wg.Add(1)
 
@@ -123,78 +179,76 @@ func main() {
 				lock.Lock()
 				defer lock.Unlock()
 
-				pythonBin := fmt.Sprintf("%s/%s/bin/python", workonHome, dirName)
-				bashCmd := fmt.Sprintf("%s --version 2>&1", pythonBin)
+				pythonBinPath := fmt.Sprintf("%s/%s/bin/python", envWorkonHome, dirName)
+				bashCommand := fmt.Sprintf("%s --version 2>&1", pythonBinPath)
+				pythonVersion, err := exec.Command("bash", "-c", bashCommand).Output()
 
-				pythonVersion, err := exec.Command("bash", "-c", bashCmd).Output()
 				if err != nil {
 					pythonVersion = []byte("???")
 				} else {
 					pythonVersion = bytes.TrimSpace(pythonVersion)
 				}
+				virtualEnvsList[dirName] = strings.Split(fmt.Sprintf("%s", pythonVersion), " ")[1]
 
-				pythonVersionStr := strings.Split(fmt.Sprintf("%s", pythonVersion), " ")[1]
-				result[dirName] = pythonVersionStr
 			}(file.Name())
-
 		}
 	}
-
 	wg.Wait()
 
-	var keys []string
-	longestKey := ""
-	for k := range result {
-		keys = append(keys, k)
-		if len(k) > len(longestKey) {
-			longestKey = k
+	var keysOfVirtualEnvsList []string
+	longestKeylength := ""
+	for key := range virtualEnvsList {
+		keysOfVirtualEnvsList = append(keysOfVirtualEnvsList, key)
+		if len(key) > len(longestKeylength) {
+			longestKeylength = key
 		}
 	}
-	sort.Strings(keys)
+	sort.Strings(keysOfVirtualEnvsList)
+	lengthOfEnvironments := len(keysOfVirtualEnvsList)
 
-	if *optionsSimpleOutput {
-		fmt.Println(strings.Join(keys[:], "\n"))
+	if *cmdOptionSimpleOutput {
+		fmt.Println(strings.Join(keysOfVirtualEnvsList[:], "\n"))
 		os.Exit(0)
 	}
 
-	envLength := len(keys)
-	envAmountStr := "environment"
-	if envLength > 1 {
-		envAmountStr += "s"
+	fmt.Printf(outputHeader,
+		printColorf(strconv.Itoa(lengthOfEnvironments), COLOR_YELLOW),
+		fmt.Sprintf(
+			"%s",
+			pluralize("virtualenv", "s", lengthOfEnvironments)))
+
+	var computedResult []string
+
+	if *cmdOptionIndexEnabled {
+		maxIndexDigits = func(number int) int {
+			if number < 9 {
+				return 1
+			} else if number > 9 {
+				return 2
+			} else if number > 99 {
+				return 3
+			} else {
+				return 6
+			}
+		}(lengthOfEnvironments)
 	}
 
-	padOffset := len(longestKey) + 5
-	indexDigits = 1
-	if envLength > 9 {
-		indexDigits = 2
-	} else if indexDigits > 99 {
-		indexDigits = 3
-	} else {
-		indexDigits = 8
-	}
-
-	if !*optionsSimpleOutput {
-		outHeader := "\nYou have %s %s available!\n\n"
-
-		if *optionsColorOutput {
-			fmt.Printf(outHeader, colorPrint(COLOR_YELLOW, strconv.Itoa(envLength)), envAmountStr)
-		} else {
-			fmt.Printf(outHeader, strconv.Itoa(envLength), envAmountStr)
+	for index, environmentName := range keysOfVirtualEnvsList {
+		indexString := ""
+		if *cmdOptionIndexEnabled {
+			indexString = fmt.Sprintf("%s ", printColorf(dynamicDigitPadding(index), COLOR_GREEN))
 		}
+		appendValue := fmt.Sprintf(
+			"%s%s %s",
+			indexString,
+			rightPaddingWithChar(
+				environmentName,
+				len(longestKeylength)+5, "",
+				fmt.Sprintf("[%s] %s", printColorf("%s", COLOR_YELLOW), printColorf("%s", COLOR_GRAY))),
+			printColorf(virtualEnvsList[environmentName], COLOR_WHITE))
+		computedResult = append(computedResult, appendValue)
 	}
 
-	for index, envName := range keys {
-		pythonVersionStr := result[envName]
-
-		if *optionsColorOutput {
-			pythonVersionStr = colorPrint(COLOR_WHITE, result[envName])
-		}
-
-		if *optionsIndexEnabled {
-			needIndex := calculateIndex(index)
-			fmt.Printf("%s %v %v\n", needIndex, rightPadding(envName, padOffset), pythonVersionStr)
-		} else {
-			fmt.Printf("%v %v\n", rightPadding(envName, padOffset), pythonVersionStr)
-		}
-	}
+	fmt.Println(strings.Join(computedResult[:], "\n"))
+	os.Exit(0)
 }
